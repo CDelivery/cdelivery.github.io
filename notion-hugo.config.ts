34d14a4394e6eb56354953c70a17b94bab63da05
cl0wn <hakomori64@gmail.com>
2023-01-09 15:45:47 +0900

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://ordinary-plantain-9ea.notion.site/017572cd57534db78df5cd19de3eaa72',
         pages: [
             // {
             //     page_id: '<page_id>',
